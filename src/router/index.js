c86b784123383fc6cdacad72eb521a8388503150
Anis Akhmedov <akhmedovanis5@gmail.com>
2025-05-28 20:24:30 +0500

    asd

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
-import useCourses from "@/stores/*";
+import useCourses from '@/stores/courses';
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
